a5c93d2de7128b939f2e4c999f5c7de777a88193
yojijuku5 <selectromoo51@gmail.com>
2025-10-27 01:11:59 +0900

    messing around with map editing

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1222,9 +1222,9 @@
 #define FLAG_ITEM_SAFARI_ZONE_NORTH_EAST_NUGGET                     0x491
 #define FLAG_ITEM_SAFARI_ZONE_SOUTH_EAST_BIG_PEARL                  0x492
 
-#define FLAG_UNUSED_0x493                                           0x493 // Unused Flag
-#define FLAG_UNUSED_0x494                                           0x494 // Unused Flag
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
+#define FLAG_ITEM_LITTLEROOT_TOWN_RARE_CANDY                        0x493 // Unused Flag
+#define FLAG_HIDE_LITTLEROOT_TOWN_SPINDA_POKEBALL                   0x494 // Unused Flag
+#define FLAG_RECEIVED_SPINDA                                        0x495 // Unused Flag
 #define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
 #define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
